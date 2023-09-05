15c57545a924be2cac9f099f46cc23a0e7ac6a65
mengstrand <michael.d.engstrand1@gmail.com>
2023-09-05 11:23:55 -0500

    slight update to seed file

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "gary1234",
+  password: "Mike54321",
   database: "employees"
 });
 
--- db/seed.sql
+++ db/seed.sql
@@ -7,3 +7,22 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+
+-- setup each role with their salary and id
+-- INSERT INTO role
+--     (title, salary, department_id)
+-- VALUES
+--     ('Sales Lead', 100000, 1),
+--     ('Salesperson',80000, 2),
+--     ('Lead Engineer',150000, 3),
+--     ('Sofware Engineer',120000, 4),
+--     ('Account Manager',160000, 5),
+--     ('Accountant',125000, 6),
+--     ('Legal Team Lead',250000, 7),
+--     ('Lawyer',190000, 8),
+
+-- setup each employee with their name, role_id and Manager ID
+-- INSERT INTO role
+--     (fname, lname, role_id, manager_id)
+-- VALUES
+--     ('John', 'Doe', )
